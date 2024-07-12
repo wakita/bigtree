9715015bb7d11d93037b4d28becc6deb36007dcc
Ken Wakita <wakita@is.titech.ac.jp>
2024-07-12 20:50:28 +0900

    ...

--- src/test/test_tol.py
+++ src/test/test_tol.py
@@ -4,6 +4,8 @@
 #    python3 src/test/test_tol.py
 # 後者の方法だと、cd src/test したあとで、単に python3 test_tol.py としても実行できる。
 
+import json
+
 import bigtree.tol
 
 ToL = bigtree.tol.TreeOfLife()
@@ -14,5 +16,5 @@ print(ToL.life(n=HomoSapiens['n']))
 print(ToL.subtree(name='Homo sapiens'))
 print(ToL.subtree(name='Homo'))
 print(ToL.life(n=HomoSapiens['parent']))
-print(ToL.subtree(name='Biota', depth=5))
-print(ToL.subtrees(ns=ToL.life(name='Biota')['children'], depth=1))
+print(json.dumps(ToL.subtree(name='Mammalia', depth=3), indent=4))
+print(ToL.subtrees(ns=ToL.life(name='Biota')['children'], depth=0))
